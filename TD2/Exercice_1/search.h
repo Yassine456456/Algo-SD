a9b8185cd9d5b596d19a38e84271b9445dcd6f86
Yassine456456 <yassineelhafoudi@gmail.com>
2025-10-22 17:35:17 +0200

    Add files via upload

---
+++ TD2/Exercice_1/search.h
@@ -0,0 +1,4 @@
+int linear_search(int *arr, int n, int target);
+int jump_search(int *arr, int n, int target);
+int binary_search(int *arr, int n, int target);
+
---
+++ TD2/Exercice_1/utlis.c
@@ -0,0 +1,16 @@
+#include "utils.h"
+
+bool is_sorted_nondecreasing(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int min_int(int a, int b)
+{
+    return (a < b) ? a : b;
+}
